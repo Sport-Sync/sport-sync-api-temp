cc6e1806415df4ca1a94116012a9e0d60cd521af
Ante Kadic <ante.kadic90@gmail.com>
2024-01-10 09:05:55 +0100

    ignore changePassword method

--- SportSync.Domain/Entities/User.cs
+++ SportSync.Domain/Entities/User.cs
@@ -55,6 +55,7 @@ public class User : Entity, IAuditableEntity, ISoftDeletableEntity
     [GraphQLIgnore]
     public bool Deleted { get; }
 
+    [GraphQLIgnore]
     public Result ChangePassword(string passwordHash)
     {
         if (passwordHash == _passwordHash)
