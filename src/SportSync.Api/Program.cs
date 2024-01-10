49296314c785dea080867806a272be60877dcc02
Ante Kadic <ante.kadic90@gmail.com>
2024-01-10 16:07:35 +0100

    cors - allow any origin

--- src/SportSync.Api/Program.cs
+++ src/SportSync.Api/Program.cs
@@ -6,41 +6,58 @@ using SportSync.GraphQL;
 using SportSync.Infrastructure;
 using SportSync.Persistence;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace SportSync.Api;
 
-builder.Services
-    .AddHttpContextAccessor()
-    .AddApplication()
-    .AddInfrastructure(builder.Configuration)
-    .AddPersistence(builder.Configuration);
-
-builder.Services.AddErrorFilter<GraphQLErrorFilter>();
-
-builder.Services
-    .AddGraphQLServer()
-    .AddProjections()
-    .AddFiltering()
-    .AddAuthorization()
-    .AddFluentValidation(x => x.UseErrorMapper((errorBuilder, context) =>
+public class Program
+{
+    public static async Task Main(string[] args)
     {
-        errorBuilder.SetMessage(context.ValidationFailure.ErrorMessage);
-    }))
-    .AddQueryType<Query>()
-    .AddMutationType<Mutation>();
-//.AddSubscriptionType<Subscription>()
-//.RegisterService<CreateUserRequestHandler>();
-//.AddGlobalObjectIdentification();
+        var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+        builder.Services
+            .AddHttpContextAccessor()
+            .AddApplication()
+            .AddInfrastructure(builder.Configuration)
+            .AddPersistence(builder.Configuration);
 
-using IServiceScope serviceScope = app.Services.CreateScope();
-using SportSyncDbContext dbContext = serviceScope.ServiceProvider.GetRequiredService<SportSyncDbContext>();
-dbContext.Database.Migrate();
+        builder.Services.AddErrorFilter<GraphQLErrorFilter>();
 
-//app.UseCustomExceptionHandler();
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseWebSockets();
-app.MapGraphQL("/api");
+        builder.Services.AddCors(x => x.AddDefaultPolicy(policy =>
+        {
+            policy.AllowAnyOrigin();
+            policy.AllowAnyHeader();
+        }));
 
-app.Run();
+        builder.Services
+            .AddGraphQLServer()
+            .AddProjections()
+            .AddFiltering()
+            .AddAuthorization()
+            .AddFluentValidation(x => x.UseErrorMapper((errorBuilder, context) =>
+            {
+                errorBuilder.SetMessage(context.ValidationFailure.ErrorMessage);
+            }))
+            .AddQueryType<Query>()
+            .AddMutationType<Mutation>();
+        //.AddSubscriptionType<Subscription>()
+        //.RegisterService<CreateUserRequestHandler>();
+        //.AddGlobalObjectIdentification();
+
+        var app = builder.Build();
+
+        using IServiceScope serviceScope = app.Services.CreateScope();
+        using SportSyncDbContext dbContext = serviceScope.ServiceProvider.GetRequiredService<SportSyncDbContext>();
+        dbContext.Database.Migrate();
+
+        app.UseCors();
+
+        //app.UseCustomExceptionHandler();
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseWebSockets();
+        app.MapGraphQL("/api");
+
+        app.Run();
+    }
+}
