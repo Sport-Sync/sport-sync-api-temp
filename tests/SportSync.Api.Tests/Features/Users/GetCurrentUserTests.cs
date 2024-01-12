276bb9f9874ec3a770db67bbed677a5418824b23
Ante Kadic <ante.kadic90@gmail.com>
2024-01-12 09:15:01 +0100

    query me - no user test

--- tests/SportSync.Api.Tests/Features/Users/GetCurrentUserTests.cs
+++ tests/SportSync.Api.Tests/Features/Users/GetCurrentUserTests.cs
@@ -8,7 +8,7 @@ namespace SportSync.Api.Tests.Features.Users;
 public class GetCurrentUserTests : IntegrationTest
 {
     [Fact]
-    public async Task Query_Me_ShouldReturnCurrentUser()
+    public async Task QueryMe_ShouldReturnCurrentUser_WhenFoundById()
     {
         var user = Database.AddUser("Ante", "Kadić", "ante.kadic@gmail.com", "092374342", "98637286423");
         await Database.SaveChangesAsync();
@@ -23,5 +23,21 @@ public class GetCurrentUserTests : IntegrationTest
         var userResponse = result.ToObject<User>("me");
 
         userResponse.FirstName.Should().Be("Ante");
+    }
+
+    [Fact]
+    public async Task QueryMe_ShouldReturnNullUser_WhenNotFoundById()
+    {
+        UserIdentifierMock.Setup(x => x.UserId).Returns(Guid.NewGuid);
+
+        var result = await ExecuteRequestAsync(
+            q => q.SetQuery(@"query{
+                me{
+                    firstName
+                }
+            }"));
+        var userResponse = result.ToObject<User>("me");
+
+        userResponse.Should().BeNull();
     }
 }
