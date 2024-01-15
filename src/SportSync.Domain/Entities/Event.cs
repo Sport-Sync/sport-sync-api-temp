5498db0561da8764dd49590641551030703ee701
akadic <a.kadic@amygdalab.net>
2024-01-15 10:22:49 +0100

    event aggregate

---
+++ src/SportSync.Domain/Entities/Event.cs
@@ -0,0 +1,20 @@
+﻿using HotChocolate;
+using SportSync.Domain.Core.Abstractions;
+using SportSync.Domain.Core.Primitives;
+
+namespace SportSync.Domain.Entities;
+
+public class Event : AggregateRoot, IAuditableEntity, ISoftDeletableEntity
+{
+    [GraphQLIgnore]
+    public DateTime CreatedOnUtc { get; set; }
+
+    [GraphQLIgnore]
+    public DateTime? ModifiedOnUtc { get; set; }
+
+    [GraphQLIgnore]
+    public DateTime? DeletedOnUtc { get; set; }
+
+    [GraphQLIgnore]
+    public bool Deleted { get; }
+}
