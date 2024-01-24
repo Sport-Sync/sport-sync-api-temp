f01fc0725cc40ecf546768621cff0fc3825293a8
Ante Kadic <ante.kadic90@gmail.com>
2024-01-24 16:13:08 +0100

    firebase installed

---
+++ src/SportSync.Application/Core/Abstractions/Notifications/IPushNotificationService.cs
@@ -0,0 +1,8 @@
+﻿using SportSync.Domain.Entities;
+
+namespace SportSync.Application.Core.Abstractions.Notifications;
+
+public interface IPushNotificationService
+{
+    Task NotifyAboutEventCreated(Event @event);
+}
