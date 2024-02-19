42b9e3082a65d36aecb399a23c4798607992025f
Ante Kadic <ante.kadic90@gmail.com>
2024-02-19 16:34:03 +0100

    unit test

---
+++ tests/SportSync.Api.Tests/Features/Users/GetUsersByPhoneNumbersTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using SportSync.Api.Tests.Common;
+using SportSync.Api.Tests.Extensions;
+using SportSync.Application.Users.GetByPhoneNumbers;
+
+namespace SportSync.Api.Tests.Features.Users;
+
+[Collection("IntegrationTests")]
+public class GetUsersByPhoneNumbersTests : IntegrationTest
+{
+    [Fact]
+    public async Task GetByPhoneNumbers_ShouldReturn_WhenFound()
+    {
+        var currentUser = Database.AddUser();
+        var user1 = Database.AddUser(phone: "0919279259");
+        var user2 = Database.AddUser(phone: "099927 9259");
+        var user3 = Database.AddUser(phone: "+385 99927 3333");
+        var user4 = Database.AddUser(phone: "0929279259");
+
+        await Database.SaveChangesAsync();
+        UserIdentifierMock.Setup(x => x.UserId).Returns(currentUser.Id);
+
+        var result = await ExecuteRequestAsync(
+            q => q.SetQuery(@"
+                query{
+                    usersByPhoneNumbers(input: {phoneNumbers: [""0959279259"", ""0919279259"", ""099927 9259"", ""+385 99927 3333""]}){
+                        users{
+                            firstName, email, phone, id
+                        }
+                    }
+                }"));
+
+        var userResponse = result.ToResponseObject<GetUsersByPhoneNumbersResponse>("usersByPhoneNumbers");
+
+        userResponse.Users.Count.Should().Be(3);
+        userResponse.Users.FirstOrDefault(x => x.Id == user1.Id).Should().NotBeNull();
+        userResponse.Users.FirstOrDefault(x => x.Id == user2.Id).Should().NotBeNull();
+        userResponse.Users.FirstOrDefault(x => x.Id == user3.Id).Should().NotBeNull();
+    }
+}
