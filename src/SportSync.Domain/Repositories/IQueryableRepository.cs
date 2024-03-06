7b4e9256d3bfd054101d3e5d9da826055a94f77e
Ante Kadic <ante.kadic90@gmail.com>
2024-03-06 12:08:58 +0100

    remove includes parameter

--- src/SportSync.Domain/Repositories/IQueryableRepository.cs
+++ src/SportSync.Domain/Repositories/IQueryableRepository.cs
@@ -4,5 +4,5 @@ namespace SportSync.Domain.Repositories;
 
 public interface IQueryableRepository<TEntity, TQueryableType>
 {
-    IQueryable<TQueryableType> GetQueryable(Expression<Func<TEntity, bool>> where = null, Expression<Func<TEntity, object>>[] includes = null);
+    IQueryable<TQueryableType> GetQueryable(Expression<Func<TEntity, bool>> where = null);
 }
--- src/SportSync.Persistence/Repositories/QueryableGenericRepository.cs
+++ src/SportSync.Persistence/Repositories/QueryableGenericRepository.cs
@@ -16,14 +16,9 @@ public abstract class QueryableGenericRepository<TEntity, TQueryableType> : Gene
         _propertySelector = propertySelector;
     }
 
-    public virtual IQueryable<TQueryableType> GetQueryable(Expression<Func<TEntity, bool>> where = null, Expression<Func<TEntity, object>>[] includes = null)
+    public virtual IQueryable<TQueryableType> GetQueryable(Expression<Func<TEntity, bool>> where = null)
     {
         var query = DbContext.Set<TEntity>().AsNoTracking().AsQueryable();
-
-        if (includes != null)
-        {
-            query = includes.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
-        }
 
         if (where is not null)
         {
