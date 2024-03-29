c09e0729f6324c21ef6c4ec069aed3812a9ad6e9
Ante Kadic <ante.kadic90@gmail.com>
2024-03-29 10:33:01 +0100

    search fix

--- src/SportSync.Application/Core/Common/PaginationInput.cs
+++ src/SportSync.Application/Core/Common/PaginationInput.cs
@@ -4,13 +4,14 @@ namespace SportSync.Application.Core.Common;
 
 public abstract class PaginationInput
 {
+    private int _firstPageSize = PaginationConstants.FirstPageSize;
     private int _pageSize;
     public int Page { get; set; }
-    public int? FirstPageSize { get; set; } = PaginationConstants.FirstPageSize;
+    public int? FirstPageSize { get; set; }
 
     public int PageSize
     {
-        get => Page == 1 ? FirstPageSize.Value : _pageSize;
+        get => Page == 1 ? FirstPageSize ?? _firstPageSize : _pageSize;
         set => _pageSize = value;
     }
 }
