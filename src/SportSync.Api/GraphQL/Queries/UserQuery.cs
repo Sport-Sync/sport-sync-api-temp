597f48182f2473318f3c3e6644324831499b7e47
akadic <a.kadic@amygdalab.net>
2024-04-12 21:22:28 +0200

    unit test

--- src/SportSync.Api/GraphQL/Queries/UserQuery.cs
+++ src/SportSync.Api/GraphQL/Queries/UserQuery.cs
@@ -2,7 +2,6 @@
 using SportSync.Application.Core.Common;
 using SportSync.Application.Users.GetByPhoneNumbers;
 using SportSync.Application.Users.GetCurrentUser;
-using SportSync.Application.Users.GetProfileImageUrl;
 using SportSync.Application.Users.GetUserProfile;
 using SportSync.Application.Users.GetUsers;
 using SportSync.Domain.Types;
@@ -21,12 +20,6 @@ public class UserQuery
     public async Task<GetPhoneBookUsersResponse> GetPhoneBookUsers(
         [Service] GetPhoneBookUsersRequestHandler requestHandler,
         GetPhoneBookUsersInput input,
-        CancellationToken cancellationToken) => await requestHandler.Handle(input, cancellationToken);
-
-    [Authorize]
-    public async Task<ProfileImageUrlResponse> GetUserProfileImageUrl(
-        [Service] ProfileImageUrlRequestHandler requestHandler,
-        ProfileImageUrlInput input,
         CancellationToken cancellationToken) => await requestHandler.Handle(input, cancellationToken);
 
     [Authorize]
--- tests/SportSync.Api.Tests/Features/Users/GetUserProfileTests.cs
+++ tests/SportSync.Api.Tests/Features/Users/GetUserProfileTests.cs
@@ -3,6 +3,7 @@ using SportSync.Api.Tests.Common;
 using SportSync.Api.Tests.Extensions;
 using SportSync.Application.Users.GetUserProfile;
 using SportSync.Domain.Core.Errors;
+using SportSync.Domain.Entities;
 
 namespace SportSync.Api.Tests.Features.Users;
 
@@ -262,5 +263,72 @@ public class GetUserProfileTests : IntegrationTest
         profileResponse.User.MutualFriends.Count.Should().Be(2);
         profileResponse.User.MutualFriends.FirstOrDefault(x => x.Id == mutualFriend1.Id).Should().NotBeNull();
         profileResponse.User.MutualFriends.FirstOrDefault(x => x.Id == mutualFriend2.Id).Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task GetUserProfile_ShouldReturn_MatchApplications()
+    {
+        var currentUser = Database.AddUser();
+        var otherUser = Database.AddUser();
+        var requestProfile = Database.AddUser("Michael", "Scott");
+
+        var match1 = Database.AddMatch(currentUser, startDate: DateTime.Today.AddDays(1));
+        var match2 = Database.AddMatch(currentUser, startDate: DateTime.Today.AddDays(1));
+        var acceptedMatch = Database.AddMatch(currentUser, startDate: DateTime.Today.AddDays(1));
+        var notAnnouncedMatch = Database.AddMatch(currentUser, startDate: DateTime.Today.AddDays(1));
+        var matchByOtherUser = Database.AddMatch(otherUser, startDate: DateTime.Today.AddDays(1));
+
+        await Database.SaveChangesAsync();
+
+        match1.Announce(currentUser, true, 1);
+        match2.Announce(currentUser, true, 1);
+        acceptedMatch.Announce(currentUser, true, 1);
+        matchByOtherUser.Announce(otherUser, true, 1);
+        
+        var application1Result = match1.ApplyForPlaying(requestProfile);
+        var application2Result = match2.ApplyForPlaying(requestProfile);
+        var application3Result = acceptedMatch.ApplyForPlaying(requestProfile);
+        var applicationForOtherUser = matchByOtherUser.ApplyForPlaying(requestProfile);
+        
+        Database.DbContext.Set<MatchApplication>().Add(application1Result.Value);
+        Database.DbContext.Set<MatchApplication>().Add(application2Result.Value);
+        Database.DbContext.Set<MatchApplication>().Add(applicationForOtherUser.Value);
+
+        application3Result.Value.Accept(currentUser, acceptedMatch, DateTime.UtcNow);
+
+        await Database.SaveChangesAsync();
+        
+        UserIdentifierMock.Setup(x => x.UserId).Returns(currentUser.Id);
+
+        var result = await ExecuteRequestAsync(
+            q => q.SetQuery(@$"
+                query{{
+                    userProfile(input: {{userId: ""{requestProfile.Id}""}}){{
+                        user{{
+                            id, firstName, lastName, email, phone, imageUrl,
+                            pendingFriendshipRequestId,
+                            isFriendWithCurrentUser,
+                            isFriendshipRequestSentByCurrentUser,
+                            mutualFriends{{
+                                id, firstName, lastName, imageUrl
+                            }}
+                        }},
+                        matchApplications{{
+                            matchId, matchApplicationId, matchName
+                        }}
+                    }}
+                }}"));
+
+        var profileResponse = result.ToResponseObject<UserProfileResponse>("userProfile");
+
+        profileResponse.MatchApplications.Count.Should().Be(2);
+        var match1Application = profileResponse.MatchApplications.FirstOrDefault(x => x.MatchId == match1.Id);
+        var match2Application = profileResponse.MatchApplications.FirstOrDefault(x => x.MatchId == match2.Id);
+
+        match1Application.MatchApplicationId.Should().Be(application1Result.Value.Id);
+        match1Application.MatchName.Should().Be(match1.EventName);
+
+        match2Application.MatchApplicationId.Should().Be(application2Result.Value.Id);
+        match2Application.MatchName.Should().Be(match2.EventName);
     }
 }
